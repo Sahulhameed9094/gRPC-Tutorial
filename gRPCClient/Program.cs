de8cb36cb54f2eecb8839da9d2de6672b8af1dcb
sahulhameed.s <sahulhameed.s@bthinq.com>
2024-05-28 15:56:42 +0530

    working project

---
+++ gRPCClient/Program.cs
@@ -0,0 +1,12 @@
+﻿using Grpc.Net.Client;
+using gRPCClient;
+
+
+
+GrpcChannel grpcChannel = GrpcChannel.ForAddress("https://localhost:7240");
+var client = new Greeter.GreeterClient(grpcChannel);
+
+HelloRequest request = new HelloRequest() { Name = "sahul hameed.s" };
+HelloReply response = client.SayHello(request);
+Console.WriteLine(response.Message);
+Console.ReadLine();
